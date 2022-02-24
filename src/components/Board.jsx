3639beda5e0194f30f3a5e52fe48955e00e872d7
saeid abdollahi <saeid.abdollahi1996@gmail.com>
2022-02-24 14:12:00 +0330

    improve styles
    Signed-off-by: saeid abdollahi <saeid.abdollahi1996@gmail.com>

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,3 +1,9 @@
 export const Board = ({ boardValue }) => {
-  return <div className="board">{boardValue}</div>;
+  return (
+    <>
+      <div className="display">
+        <input type="text" placeholder="0" className="displayResult" readOnly value={boardValue}/>
+      </div>
+    </>
+  );
 };
--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import { AadadButtons } from "./AadadButtons";
 import { Board } from "./Board";
-import "./calculator.css"
+import "./calculator.css";
 
 function Calculator() {
   const [boardValue, setBoardValue] = useState("0");
   const onAadadClickHandler = (e) => {
+    e.preventDefault();
     let value = e.target.textContent;
     if (value === "C") {
       setBoardValue("0");
@@ -15,10 +16,12 @@ function Calculator() {
   };
   return (
     <>
-      <form className="calculatorFrom">
-        <Board boardValue={boardValue} />
-        <AadadButtons onAadadClickHandler={onAadadClickHandler} />
-      </form>
+      <div className="container" dir="rtl">
+        <form className="calculatorFrom">
+          <Board boardValue={boardValue} />
+          <AadadButtons onAadadClickHandler={onAadadClickHandler} />
+        </form>
+      </div>
     </>
   );
 }

503e6edeaa24316b3735d98ce5860b55604be552
saeid abdollahi <saeid.abdollahi1996@gmail.com>
2022-02-24 15:16:44 +0330

    some refactor styles and add equal handler
    Signed-off-by: saeid abdollahi <saeid.abdollahi1996@gmail.com>

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,7 +1,7 @@
 export const Board = ({ boardValue }) => {
   return (
     <>
-      <div className="display">
+      <div className="display" dir="ltr">
         <input type="text" placeholder="0" className="displayResult" readOnly value={boardValue}/>
       </div>
     </>
--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,25 +1,36 @@
 import { useState } from "react";
-import { AadadButtons } from "./AadadButtons";
+import { AadadButtons } from "./Buttons";
 import { Board } from "./Board";
 import "./calculator.css";
 
 function Calculator() {
   const [boardValue, setBoardValue] = useState("0");
-  const onAadadClickHandler = (e) => {
+  const onNumberClickHandler = (e) => {
     e.preventDefault();
     let value = e.target.textContent;
+    if (boardValue[0] === "0") {
+      setBoardValue((preValue) => preValue.slice(1) + e.target.textContent);
+      return;
+    }
     if (value === "C") {
       setBoardValue("0");
       return;
     }
     setBoardValue((preValue) => preValue + e.target.textContent);
   };
+  const onEqualClickHandler = (e) => {
+    e.preventDefault();
+    setBoardValue(Calculate(boardValue));
+  };
   return (
     <>
       <div className="container" dir="rtl">
         <form className="calculatorFrom">
           <Board boardValue={boardValue} />
-          <AadadButtons onAadadClickHandler={onAadadClickHandler} />
+          <AadadButtons
+            onNumberClickHandler={onNumberClickHandler}
+            onEqualClickHandler={onEqualClickHandler}
+          />
         </form>
       </div>
     </>
@@ -27,3 +38,7 @@ function Calculator() {
 }
 
 export default Calculator;
+
+const Calculate = (boardValue) => {
+  return eval(boardValue);
+};
